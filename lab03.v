ecca140e74621ae950cb5ef11f3802d603ee5d5e
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2022-03-20 21:28:46 +0300

    Do 5 out of 7 tasks

---
+++ lab03.v
@@ -0,0 +1,47 @@
+Section lab03.
+
+Variables A B C : Prop.
+
+(* Exercise 1: Prove implication is transitive. What does this logical lemma correspond to in functional programming? *)
+Check
+  (fun (aIb : (A -> B)) (bIc : (B -> C)) => 
+  (fun (a:A) => bIc (aIb a)))
+: (A -> B) -> (B -> C) -> (A -> C).
+
+(* Exercise 2: Prove conjunction is associative *)
+Check
+  (fun aAbAc => 
+  conj (proj1 (proj1 aAbAc)) (conj (proj2 (proj1 aAbAc)) (proj2 aAbAc)))
+: (A /\ B) /\ C -> A /\ (B /\ C).
+
+(* Exercise 3: Prove disjunction distributes over conjunction: *)
+Check
+  (fun aVbAc => conj (match aVbAc with 
+  | or_introl a => or_introl a
+  | or_intror bAc => or_intror (proj1 bAc)
+end) (match aVbAc with 
+  | or_introl a => or_introl a
+  | or_intror bAc => or_intror (proj2 bAc)
+end)
+) : A \/ (B /\ C) -> (A \/ B) /\ (A \/ C).
+
+(* Exercise 4: Prove weak form of Peirce's law holds in intuitionistic logic *)
+Check
+  (_)
+: ((((A -> B) -> A) -> A) -> B) -> B.
+
+(* Exercise 5: We can always add double negation (but cannot drop it in general) *)
+Check
+  (fun a => (fun b => b a)) 
+: A -> ~ ~ A.
+
+(* Exercise 6: Although we can in some special cases like the following: *)
+Check
+  (fun f a => f (fun b => b a))
+: ~ ~ ~ A -> ~ A.
+
+(* Exercise 7: Prove we cannot add the negation of the law of excluded middle and have a sound logic.
+   Keep in mind that "~ A" means "A -> False" *)
+Check
+  (_)
+: ~ ~ (A \/ ~ A).
